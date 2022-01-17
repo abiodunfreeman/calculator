cf52d852c13bffaa89cc6f60e669cc56c1afebd6
Odun <bornduringwar@gmail.com>
2022-01-16 22:34:11 -0500

    cleaned up code a lot, made improvements to how calculator functions

--- calc2/javascript.js
+++ calc2/javascript.js
@@ -41,9 +41,13 @@ let valueB  = '';
 let valueOp = '';
 btnsNum.forEach( (btn) => {
     btn.addEventListener('click', (e) => {
-        
+       if (isEmpty(valueOp) === false && isEmpty(valueB) === false) {
+           valueA += e.target.textContent
+           displayValue.textContent = `${valueB} ${valueOp} ${valueA}`
+       } else {
         valueA += e.target.textContent
-        displayValue.textContent = valueA;
+        displayValue.textContent = `${valueB} ${valueOp} ${valueA}`
+       }
     })
 })
 
@@ -51,49 +55,42 @@ btnsOp.forEach( (btn) => {
     btn.addEventListener('click', (e) => {
         if (e.target.textContent === '=') {
             if (valueOp === '' || valueB === '' || valueA === '') {
+                displayValue.textContent = valueB
                 console.log(`Operation = ${valueOp} || Value A = ${valueA} || Value B = ${valueB}`)
-            } else {
-            console.log('EQUAL CHOSE')
+             } else {
+                bValueDisplay.textContent = `${valueB} ${valueOp} ${valueA}`
             valueB = operation(valueOp, valueB, valueA)
+            displayValue.textContent = valueB + ' ' + valueOp;
             valueA = ''
-            bValueDisplay.textContent = ''
-            displayValue.textContent = valueB
             console.log(`Operation = ${valueOp} || Value A = ${valueA} || Value B = ${valueB}`)
 
         }
         } else if (e.target.textContent === '+') {
-            valueOp = '+';
-            valueB = valueA
-            valueA = '';
-            bValueDisplay.textContent = valueB + ' ' + valueOp;
-         
+            opFunc(e.target.textContent)
         } else if (e.target.textContent === '-') {
-            if (valueOp === "") {
-            valueOp = '-'
-            valueB = valueA
-            valueA = ''
-            bValueDisplay.textContent = valueB + ' ' + valueOp;
-            console.log(`Operation = ${valueOp} || Value A = ${valueA} || Value B = ${valueB}`)
-            } else {
-                valueB = operation(valueOp, valueB, valueA)
-                valueOp = '-'
-                bValueDisplay.textContent = valueB + " " + valueOp;
-                valueA = "" ;
-            }
+            opFunc(e.target.textContent)
         } else if (e.target.textContent === 'x') {
-            valueOp = 'x'
-            valueB = valueA;
-            valueA = '';
-            bValueDisplay.textContent = valueB + ' ' + valueOp;
+            opFunc(e.target.textContent)
         } else if (e.target.textContent === '÷') {
-            valueOp = '÷';
-            valueB = valueA
-            valueA = '';
-            bValueDisplay.textContent = valueB + ' ' + valueOp;
+            opFunc(e.target.textContent)
         }
     })
 })
+const opFunc = function (operator) { //changes valueOp to selected operator , updates displayValue with new op and value B
+    valueOp = operator
+    if (isEmpty(valueA) === true ) { // if then statement that check if valueA is empty if it is we only update the valueOp and display
+        displayValue.textContent = valueB + ' ' + valueOp;
+    } else {
+    valueB = valueA
+    valueA = ''
+    displayValue.textContent = valueB + ' ' + valueOp;
+    console.log(`Operation = ${valueOp} || Value A = ${valueA} || Value B = ${valueB}`)
+    }
 
+}
+const isEmpty = function (obj) { //checks to see if object is empty returns true if it is
+    return Object.keys(obj).length === 0;
+}
 clear.addEventListener('click', (e) => {
     console.log('clear')
     valueA = ''
